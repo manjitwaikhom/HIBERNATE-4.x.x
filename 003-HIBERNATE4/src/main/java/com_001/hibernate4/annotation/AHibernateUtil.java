fc48dbf6d51febada4314d842736047fe851f47b
manjitwaikhom <manjitwaikhom@gmail.com>
2016-11-16 12:59:04 +0530

    update

--- 003-HIBERNATE4/src/main/java/com_001/hibernate4/annotation/AHibernateUtil.java
+++ 003-HIBERNATE4/src/main/java/com_001/hibernate4/annotation/AHibernateUtil.java
@@ -1,17 +1,26 @@
 package com_001.hibernate4.annotation;
 
+import java.util.Properties;
+
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 
 public class AHibernateUtil {
 
 	static SessionFactory factory;
 	static {
 		try {
-			AnnotationConfiguration cfg = new AnnotationConfiguration();
-			cfg = (AnnotationConfiguration) cfg
+
+			Configuration cfg = new Configuration();
+			cfg = cfg
 					.configure("com_001/hibernate4/annotation/hibernate.cfg.xml");
-			factory = cfg.buildSessionFactory();
+			StandardServiceRegistryBuilder ssrBuilder = new StandardServiceRegistryBuilder();
+			Properties settings = cfg.getProperties();
+			ssrBuilder = ssrBuilder.applySettings(settings);
+			StandardServiceRegistry ssRegistry = ssrBuilder.build();
+			factory = cfg.buildSessionFactory(ssRegistry);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
