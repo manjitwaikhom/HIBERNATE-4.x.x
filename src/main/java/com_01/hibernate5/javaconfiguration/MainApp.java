c9d85558d20472c4e022d2a11160271b05e90e77
manjitwaikhom <manjitsinghwk@gmail.com>
2019-04-05 17:07:23 +0530

    Hibernate-5 XML & Java Configs

---
+++ src/main/java/com_01/hibernate5/javaconfiguration/MainApp.java
@@ -0,0 +1,19 @@
+package com_01.hibernate5.javaconfiguration;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class MainApp {
+   
+        public static void main(String[] args) {
+            StudentDao studentDao = new StudentDao();
+            Student student1 = new Student("Happy", "Singh", "happy@gmail.com");
+            Student student2 = new Student("Abhishek", "Kumar", "abhishek@gmail.com");
+            studentDao.saveStudent(student1);
+            studentDao.saveStudent(student2);
+            List < Student > students = studentDao.getStudents();
+            students.forEach(s -> System.out.println("First Name: "+s.getFirstName()+"  Last Name: "+s.getLastName()+"  Email: "+s.getEmail()));
+        }
+}
