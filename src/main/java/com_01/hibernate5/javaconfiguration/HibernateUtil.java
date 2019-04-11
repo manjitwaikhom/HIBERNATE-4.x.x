8610b99b95ab9f4671a5c59cf48c1f1687deaa16
manjitwaikhom <manjitsinghwk@gmail.com>
2019-04-11 17:49:34 +0530

    Using static imports

--- src/main/java/com_01/hibernate5/javaconfiguration/HibernateUtil.java
+++ src/main/java/com_01/hibernate5/javaconfiguration/HibernateUtil.java
@@ -6,7 +6,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
+//static import
+import static org.hibernate.cfg.Environment.*;
 import org.hibernate.service.ServiceRegistry;
 
 
@@ -81,14 +82,14 @@ public class HibernateUtil {
 				
 				// Hibernate settings equivalent to hibernate.cfg.xml's properties
 				Properties settings = new Properties();
-				settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-				settings.put(Environment.URL, "jdbc:mysql://localhost:3306/hibernate5_db?useSSL=false");
-				settings.put(Environment.USER, "root");
-				settings.put(Environment.PASS, "admin");
-				settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
-				settings.put(Environment.SHOW_SQL, "true");
-				settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-				settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+				settings.put(DRIVER, "com.mysql.cj.jdbc.Driver");
+				settings.put(URL, "jdbc:mysql://localhost:3306/hibernate5_db?useSSL=false");
+				settings.put(USER, "root");
+				settings.put(PASS, "admin");
+				settings.put(DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+				settings.put(SHOW_SQL, "true");
+				settings.put(CURRENT_SESSION_CONTEXT_CLASS, "thread");
+				settings.put(HBM2DDL_AUTO, "create-drop");
 				
 				Configuration configuration = new Configuration();
 				configuration.setProperties(settings);
