23d07ac4d6bbe64758a04c41003264c6d4150a90
manjitwaikhom <manjitsinghwk@gmail.com>
2019-04-15 13:21:29 +0530

    SQL Scripts

---
+++ Scripts/Script.sql
@@ -0,0 +1,10 @@
+show tables;
+
+select * from hibernate_sequence;
+select * from courses;
+select * from emails;
+select * from marks;
+select * from phones;
+select * fom refs;
+select * from students;
+select * from employees;
