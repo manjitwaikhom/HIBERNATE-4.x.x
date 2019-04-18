03531a7deeed85d323c96641cfa4664e7de2f3d9
manjitwaikhom <manjitsinghwk@gmail.com>
2019-04-18 13:31:12 +0530

    Minor Update

--- src/main/java/com_03/hibernate5/inheritanceMapping/tablePerSubclass/MainApp.java
+++ src/main/java/com_03/hibernate5/inheritanceMapping/tablePerSubclass/MainApp.java
@@ -10,9 +10,9 @@ public class MainApp {
 	public static void main(String as[]){
 	    
 	List<String> emails=new ArrayList<String>();
-	emails.add("manjit@jlc.com");
-	emails.add("waikhom@jlc.com");
-	emails.add("singh@jlc.com");
+	emails.add("manjit@gmail.com");
+	emails.add("waikhom@gmail.com");
+	emails.add("singh@gmail.com");
 	
 	Set<Long> phones=new HashSet<Long>();
 	phones.add(new Long(999331));
@@ -33,7 +33,7 @@ public class MainApp {
 	System.out.println(it1.intValue());
 	
 	//3.adding the old student
-	OldStudent ostu=new OldStudent(3,"sri","Blore","enabled",15000.0,emails,phones,"SDSoft","manjit@jlc.com",30000.00);
+	OldStudent ostu=new OldStudent(3,"sri","Blore","enabled",15000.0,emails,phones,"SDSoft","manjit@gmail.com",30000.00);
 	Integer it2=(Integer)HibernateTemplate.saveObject(ostu);
 	System.out.println(it2.intValue());
 	
@@ -43,7 +43,7 @@ public class MainApp {
 	System.out.println(it3.intValue());
 	
 	//5.adding the WeekendStudent
-	WeekendStudent wstu=new WeekendStudent(5,"sri","Blore","enabled",15000.0,emails,phones,2000.0, "09:00am", "Mathikhere","SDSoft","waikhom@jlc.com",9.0);
+	WeekendStudent wstu=new WeekendStudent(5,"sri","Blore","enabled",15000.0,emails,phones,2000.0, "09:00am", "Mathikhere","SDSoft","waikhom@gmail.com",9.0);
 	Integer it4=(Integer)HibernateTemplate.saveObject(wstu);
 	System.out.println(it4.intValue());
 }
